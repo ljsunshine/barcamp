1cf0def6ec309da23442c2ca63729277d4463211
ljsunshine <88727948+ljsunshine@users.noreply.github.com>
2023-08-31 02:24:39 +0200

    Update solutions.java

--- solutions.java
+++ solutions.java
@@ -4,18 +4,18 @@ interface CPU {
 }
 
 // ConcreteProducts: CPU of Brand A
-class CPU_A implements CPU {
+class IntelCPU implements CPU {
     @Override
     public String getDetails() {
-        return "CPU of Brand A";
+        return "CPU of Brand Intel";
     }
 }
 
 // ConcreteProducts: CPU of Brand B
-class CPU_B implements CPU {
+class AMDCPU implements CPU {
     @Override
     public String getDetails() {
-        return "CPU of Brand B";
+        return "CPU of Brand AMD";
     }
 }
 
@@ -25,66 +25,66 @@ interface Monitor {
 }
 
 // ConcreteProducts: Monitor of Brand A
-class Monitor_A implements Monitor {
+class IntelMonitor implements Monitor {
     @Override
     public String getDetails() {
-        return "Monitor of Brand A";
+        return "Monitor of Brand Intel";
     }
 }
 
 // ConcreteProducts: Monitor of Brand B
-class Monitor_B implements Monitor {
+class AMDMonitor implements Monitor {
     @Override
     public String getDetails() {
-        return "Monitor of Brand B";
+        return "Monitor of Brand AMD";
     }
 }
 
 // AbstractFactory: Interface for creating CPUs and Monitors
-interface ComputerFactory {
+interface ComputerPartsFactory {
     CPU createCPU();
     Monitor createMonitor();
 }
 
-// ConcreteFactory: Creates CPUs and Monitors of Brand A
-class ComputerFactoryA implements ComputerFactory {
+// ConcreteFactory: Creates CPUs and Monitors of Brand Intel
+class IntelComputerPartsFactory implements ComputerPartsFactory {
     @Override
     public CPU createCPU() {
-        return new CPU_A();
+        return new IntelCPU();
     }
 
     @Override
     public Monitor createMonitor() {
-        return new Monitor_A();
+        return new IntelMonitor();
     }
 }
 
-// ConcreteFactory: Creates CPUs and Monitors of Brand B
-class ComputerFactoryB implements ComputerFactory {
+// ConcreteFactory: Creates CPUs and Monitors of Brand AMD
+class AMDComputerPartsFactory implements ComputerPartsFactory {
     @Override
     public CPU createCPU() {
-        return new CPU_B();
+        return new AMDCPU();
     }
 
     @Override
     public Monitor createMonitor() {
-        return new Monitor_B();
+        return new AMDMonitor();
     }
 }
 
 public class Main {
     public static void main(String[] args) {
-        // Create a computer factory of Brand A
-        ComputerFactory factoryA = new ComputerFactoryA();
-        CPU cpuA = factoryA.createCPU();
-        Monitor monitorA = factoryA.createMonitor();
-        System.out.println("Brand A: " + cpuA.getDetails() + ", " + monitorA.getDetails());
+        // Create a computer factory of Brand Intel
+        ComputerPartsFactory intelFactory = new IntelComputerPartsFactory();
+        CPU intelCPU = intelFactory.createCPU();
+        Monitor intelMonitor = intelFactory.createMonitor();
+        System.out.println("Intel: " + intelCPU.getDetails() + ", " + intelMonitor.getDetails());
 
-        // Create a computer factory of Brand B
-        ComputerFactory factoryB = new ComputerFactoryB();
-        CPU cpuB = factoryB.createCPU();
-        Monitor monitorB = factoryB.createMonitor();
-        System.out.println("Brand B: " + cpuB.getDetails() + ", " + monitorB.getDetails());
+        // Create a computer factory of Brand AMD
+        ComputerPartsFactory amdFactory = new AMDComputerPartsFactory();
+        CPU amdCPU = amdFactory.createCPU();
+        Monitor amdMonitor = amdFactory.createMonitor();
+        System.out.println("AMD: " + amdCPU.getDetails() + ", " + amdMonitor.getDetails());
     }
 }
 
